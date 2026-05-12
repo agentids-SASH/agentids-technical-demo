443fe9d5391b9e36bb9a17d65db493335ae0bccd
Sam Boger <sam.boger@gmail.com>
2026-05-12 09:43:51 -0400

    typo

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base : "/agent-id-concept",
+  base : "/agent-id-concept/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
